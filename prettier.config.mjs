74f326246912b43d5c62e29ec32ad21453783365
Jose Rodrigues <jose@skrip.dev>
2025-03-24 14:50:53 -0300

    feat: prettier config

---
+++ prettier.config.mjs
@@ -0,0 +1,12 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+  tabWidth: 2,
+  semi: true,
+  singleQuote: false,
+  plugins: ["prettier-plugin-organize-imports"],
+};
+
+export default config;
